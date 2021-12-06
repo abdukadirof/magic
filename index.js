8c5ec6110bd0f59bb497bbd0e94b8c5eef5d6796
Пользователь <you@example.com>
2021-12-06 18:54:23 +0500

    men bugun navbar yasadim

---
+++ index.js
@@ -0,0 +1,10 @@
+// const list = document.querySelectorAll('.list');
+//
+// function activateLink() {
+//     list.forEach((item) =>
+//         item.classList.remove('activate'));
+//     this.classList.add('activate')
+// }
+//
+// list.forEach((item) =>
+//     item.addEventListener('click', activateLink));
